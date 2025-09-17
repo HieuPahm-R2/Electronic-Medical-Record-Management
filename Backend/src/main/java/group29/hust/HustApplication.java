29e0b59efdb357bddfca956e02fe333f3a83cd0f
HieuPahmTr <phamvanthang786@gmail.com>
2025-09-17 07:57:54 +0700

    Khởi tạo base dự án

---
+++ Backend/src/main/java/group29/hust/HustApplication.java
@@ -0,0 +1,13 @@
+package group29.hust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/group29/hust/HustApplicationTests.java
@@ -0,0 +1,13 @@
+package group29.hust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
